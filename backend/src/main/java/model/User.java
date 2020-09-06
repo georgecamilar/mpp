61d7187c22e450e2d053cdd82d44fa44404aa7be
lkuya <cgir2065@scs.ubbcluj.ro>
2020-09-06 14:05:41 +0300

    Initialize commit

---
+++ backend/src/main/java/model/User.java
@@ -0,0 +1,17 @@
+package model;
+
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(Integer id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+}
